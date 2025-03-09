c6b748a01d24d564fc27336a7f1f38056cb95a29
Vincent Ging Ho Yim <cenviity@gmail.com>
2025-03-09 23:58:41 +1100

    refactor: inline `total_amount` variable

--- python/statement.py
+++ python/statement.py
@@ -51,7 +51,6 @@ def statement(invoice: dict, plays: dict) -> str:
         # print line for this order
         result += f" {play_for(perf)['name']}: {usd(amount_for(perf))} ({perf['audience']} seats)\n"
 
-    total_amount = apple_sauce()
-    result += f"Amount owed is {usd(total_amount)}\n"
+    result += f"Amount owed is {usd(apple_sauce())}\n"
     result += f"You earned {total_volume_credits()} credits\n"
     return result
