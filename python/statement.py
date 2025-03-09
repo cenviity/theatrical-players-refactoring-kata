9cece5301e915c50f3210afa4d968c06daf7f13d
Vincent Ging Ho Yim <cenviity@gmail.com>
2025-03-09 23:01:04 +1100

    refactor: inline `play` variable

--- python/statement.py
+++ python/statement.py
@@ -31,18 +31,17 @@ def statement(invoice: dict, plays: dict) -> str:
         return result
 
     for perf in invoice["performances"]:
-        play = play_for(perf)
-        this_amount = amount_for(perf, play)
+        this_amount = amount_for(perf, play_for(perf))
 
         # add volume credits
         volume_credits += max(perf["audience"] - 30, 0)
 
         # add extra credit for every ten comedy attendees
-        if "comedy" == play["type"]:
+        if "comedy" == play_for(perf)["type"]:
             volume_credits += math.floor(perf["audience"] / 5)
 
         # print line for this order
-        result += f" {play['name']}: {format_as_dollars(this_amount / 100)} ({perf['audience']} seats)\n"
+        result += f" {play_for(perf)['name']}: {format_as_dollars(this_amount / 100)} ({perf['audience']} seats)\n"
         total_amount += this_amount
 
     result += f"Amount owed is {format_as_dollars(total_amount / 100)}\n"
