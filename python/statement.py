2b4561a064528aecdb10d5f7f461eef8a8336dad
Vincent Ging Ho Yim <cenviity@gmail.com>
2025-03-09 23:56:02 +1100

    refactor: extract function to calculate total invoice amount
    A random name is used for the extracted function here since we already have a
    `total_amount` variable.

--- python/statement.py
+++ python/statement.py
@@ -35,6 +35,12 @@ def statement(invoice: dict, plays: dict) -> str:
 
         return result
 
+    def apple_sauce() -> int:
+        total_amount = 0
+        for perf in invoice["performances"]:
+            total_amount += amount_for(perf)
+        return total_amount
+
     def total_volume_credits() -> int:
         volume_credits = 0
         for perf in invoice["performances"]:
@@ -45,10 +51,7 @@ def statement(invoice: dict, plays: dict) -> str:
         # print line for this order
         result += f" {play_for(perf)['name']}: {usd(amount_for(perf))} ({perf['audience']} seats)\n"
 
-    total_amount = 0
-    for perf in invoice["performances"]:
-        total_amount += amount_for(perf)
-
+    total_amount = apple_sauce()
     result += f"Amount owed is {usd(total_amount)}\n"
     result += f"You earned {total_volume_credits()} credits\n"
     return result
