b5934f42b1b5cf6b5edad939523999c04ea65ec6
Vincent Ging Ho Yim <cenviity@gmail.com>
2025-03-10 01:17:02 +1100

    refactor: calculate invoice amounts using intermediate data structure instead

--- python/statement.py
+++ python/statement.py
@@ -5,28 +5,11 @@ def statement(invoice: dict, plays: dict) -> str:
     def enrich_performance(performance: dict) -> dict:
         result = performance.copy()
         result["play"] = play_for(result)
+        result["amount"] = amount_for(result)
         return result
 
     def play_for(performance: dict) -> dict:
         return plays[performance["playID"]]
-
-    statement_data = {
-        "customer": invoice["customer"],
-        "performances": [enrich_performance(perf) for perf in invoice["performances"]],
-    }
-    return render_plain_text(statement_data)
-
-
-def render_plain_text(data: dict) -> str:
-    def usd(amount: float) -> str:
-        return f"${amount / 100:0,.2f}"
-
-    def volume_credits_for(performance: dict) -> int:
-        result = 0
-        result += max(performance["audience"] - 30, 0)
-        if "comedy" == performance["play"]["type"]:
-            result += math.floor(performance["audience"] / 5)
-        return result
 
     def amount_for(performance: dict) -> int:
         match performance["play"]["type"]:
@@ -46,10 +29,28 @@ def render_plain_text(data: dict) -> str:
 
         return result
 
+    statement_data = {
+        "customer": invoice["customer"],
+        "performances": [enrich_performance(perf) for perf in invoice["performances"]],
+    }
+    return render_plain_text(statement_data)
+
+
+def render_plain_text(data: dict) -> str:
+    def usd(amount: float) -> str:
+        return f"${amount / 100:0,.2f}"
+
+    def volume_credits_for(performance: dict) -> int:
+        result = 0
+        result += max(performance["audience"] - 30, 0)
+        if "comedy" == performance["play"]["type"]:
+            result += math.floor(performance["audience"] / 5)
+        return result
+
     def total_amount() -> int:
         result = 0
         for perf in data["performances"]:
-            result += amount_for(perf)
+            result += perf["amount"]
         return result
 
     def total_volume_credits() -> int:
@@ -61,7 +62,7 @@ def render_plain_text(data: dict) -> str:
     result = f"Statement for {data['customer']}\n"
 
     for perf in data["performances"]:
-        result += f" {perf['play']['name']}: {usd(amount_for(perf))} ({perf['audience']} seats)\n"
+        result += f" {perf['play']['name']}: {usd(perf['amount'])} ({perf['audience']} seats)\n"
 
     result += f"Amount owed is {usd(total_amount())}\n"
     result += f"You earned {total_volume_credits()} credits\n"
