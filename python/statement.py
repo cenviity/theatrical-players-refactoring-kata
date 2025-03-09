70b4483c052b3c560f86dad26ab8bca801a577d2
Vincent Ging Ho Yim <cenviity@gmail.com>
2025-03-09 23:52:51 +1100

    refactor: split out loop for calculating total invoice amount

--- python/statement.py
+++ python/statement.py
@@ -45,6 +45,8 @@ def statement(invoice: dict, plays: dict) -> str:
     for perf in invoice["performances"]:
         # print line for this order
         result += f" {play_for(perf)['name']}: {usd(amount_for(perf))} ({perf['audience']} seats)\n"
+
+    for perf in invoice["performances"]:
         total_amount += amount_for(perf)
 
     result += f"Amount owed is {usd(total_amount)}\n"
