88693a50051c0b5e26a63b6ad39b6f8c8a62d31f
Vincent Ging Ho Yim <cenviity@gmail.com>
2025-03-09 23:59:25 +1100

    refactor: rename `apple_sauce` to `total_amount`

--- python/statement.py
+++ python/statement.py
@@ -35,7 +35,7 @@ def statement(invoice: dict, plays: dict) -> str:
 
         return result
 
-    def apple_sauce() -> int:
+    def total_amount() -> int:
         total_amount = 0
         for perf in invoice["performances"]:
             total_amount += amount_for(perf)
@@ -51,6 +51,6 @@ def statement(invoice: dict, plays: dict) -> str:
         # print line for this order
         result += f" {play_for(perf)['name']}: {usd(amount_for(perf))} ({perf['audience']} seats)\n"
 
-    result += f"Amount owed is {usd(apple_sauce())}\n"
+    result += f"Amount owed is {usd(total_amount())}\n"
     result += f"You earned {total_volume_credits()} credits\n"
     return result
